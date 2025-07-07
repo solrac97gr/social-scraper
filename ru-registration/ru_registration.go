f608be449535ee9967982248dbd648ab25fc9c6c
Carlos García Rosales <carlosgarcia_97gr@hotmail.com>
2025-07-07 20:59:48 +0200

    remove debugging link line

--- ru-registration/ru_registration.go
+++ ru-registration/ru_registration.go
@@ -11,7 +11,6 @@ var pendingChecks int32
 
 // CheckRegistrationStatus checks if the given link is registered on the specified website
 func CheckRegistrationStatus(link string, semaphore chan struct{}) bool {
-	println("Checking registration status for:", link)
 	atomic.AddInt32(&pendingChecks, 1)
 	log.Printf("Checking registration status for: %s (Pending checks: %d)", link, atomic.LoadInt32(&pendingChecks))
 	cmd := exec.Command("node", "scripts/ru-registration.js", link)
